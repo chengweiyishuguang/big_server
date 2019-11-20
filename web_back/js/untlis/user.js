900c30882b482661d3fb231d07eb7d669521a183
lm <984425247@qq.com>
2019-11-20 16:46:25 +0800

    优化完成

--- web_back/js/untlis/user.js
+++ web_back/js/untlis/user.js
@@ -1,44 +1,30 @@
 var basrUrl = 'http://localhost:8000'
 var user = {
-    login: function(myname, mypassword) {
+    login: function(myname, mypassword, callback) {
         $.post(basrUrl + '/admin/login', {
                 user_name: myname,
                 password: mypassword,
             },
             function(res) {
                 // console.log(res);
-                if (res.code === 200) {
-                    location.href = 'index.html'
-
-                } else {
-                    $('#msgInfo').text('不能为空')
-                    $('#myModal').modal('show')
-                }
+                callback(res)
             }
         )
 
     },
-    loginOut: function() {
+    loginOut: function(callback) {
         $.post(basrUrl + '/admin/logout', function(res) {
             // console.log(res);
-            if (res.code === 200) {
-                window.location.href = 'login.html'
-            }
+            callback(res)
+
 
 
         })
 
     },
-    logininfo: function() {
+    logininfo: function(callback) {
         $.get(basrUrl + '/admin/getuser', function(res) {
-            // console.log(res);
-            if (res.code === 200) {
-                $("#userImg").prop('src', res.data.user_pic);
-                $("#userName").text(res.data.nickname);
-            }
-
-            // var myname=$("#userImg").val
-
+            callback(res)
 
         })
     }
